3959153c25ac7f3bc38ed934f8411a83321796df
Rachelle Herrera <rachelle.herera@gmail.com>
2015-06-28 20:49:07 -0700

    implemented player name box

--- tictactoe.js
+++ tictactoe.js
@@ -1,3 +1,8 @@
+function setPlayerName(idOfDiv, playerNameDiv) {
+  playerName = document.getElementById(idOfDiv).value;
+  document.getElementById(playerNameDiv).innerHTML = playerName
+}
+
 var cells = ['','','','','','','','',''];
 var turn = 0; 
 var gameBoard = [
@@ -17,37 +22,61 @@ function playerMove(index) {
   if (player === 'X') {
       document.getElementById(index).innerHTML = "<img id='pikachu' src='pikachumove.gif'>"
       player = 'O';
-      //TODO:
-      //write a function that checks winner
-      //call it every click
-      cells[index] = index;
+      cells[index] = 'X';
       checkWinner();
     } else {
       document.getElementById(index).innerHTML = "<img id='charmander' src='charmander.gif'>"
       player = 'X';
-      cells[index] = index;
+      cells[index] = 'O'
       checkWinner();
     }
 }
 
-
 function checkWinner() { 
-  
-	for(var i = 0; i < gameBoard.length; i++){ 
-    var combo = gameBoard[i];
-    console.log(combo[2])
-    // if ((cells[combo[0]]==cells[combo[1]]) && (cells[combo[1]]==cells[combo[2]]) && (cells[combo[0]]!="")) {
-    //   alert("you win, " + cells[combo[0]] + "!");
+  if(cells[0] === "X" && cells[1] === "X" && cells[2] === "X"){
+        alert("Pikachu Wins!");
+     }else if(cells[3] === "X" && cells[4] === "X" && cells[5] === "X"){
+        alert("Pikachu Wins!");
+     }else if(cells[6] === "X" && cells[7] === "X" && cells[8] === "X"){
+        alert("Pikachu Wins!"); 
+     }else if(cells[0] === "X" && cells[3] === "X" && cells[6] === "X"){
+        alert("Pikachu Wins!");
+     }else if(cells[1] === "X" && cells[4] === "X" && cells[7] === "X"){
+        alert("Pikachu Wins!"); 
+     }else if(cells[2] === "X" && cells[5] === "X" && cells[8] === "X"){
+        alert("Pikachu Wins!");
+     }else if(cells[0] === "X" && cells[4] === "X" && cells[8] === "X"){
+        alert("Pikachu Wins!");
+     }else if(cells[2] === "X" && cells[4] === "X" && cells[6] === "X"){
+        alert("Pikachu Wins!");
+     }else if(cells[0] === "O" && cells[1] === "O" && cells[2] === "O"){
+        alert("Charmander Wins!");
+     }else if(cells[3] === "O" && cells[4] === "O" && cells[5] === "O"){
+        alert("Charmander Wins!");
+     }else if(cells[6] === "O" && cells[7] === "O" && cells[8] === "O"){
+        alert("Charmander Wins!");
+     }else if(cells[0] === "O" && cells[3] === "O" && cells[6] === "O"){
+        alert("Charmander Wins!");
+     }else if(cells[1] === "O" && cells[4] === "O" && cells[7] === "O"){
+        alert("Charmander Wins!"); 
+     }else if(cells[2] === "O" && cells[5] === "O" && cells[8] === "O"){ 
+        alert("Charmander Wins!");
+     }else if(cells[0] === "O" && cells[4] === "O" && cells[8] === "O"){
+        alert("Charmander Wins!"); 
+     }else if(cells[2] === "O" && cells[4] === "O" && cells[6] === "O"){
+        alert("Charmander Wins!");
 
-    // }
-  }
+   }
+}
+   function catsGame(){
+      
+   }
 
  //  if(checkWinner === gameboard[i]){
  // 	  console.log("You have won the battle " + checkWinner + " !"); 
  // }
  //    return checkWinner();
  
- } 
 
 
  // var playerWins = 0; 
